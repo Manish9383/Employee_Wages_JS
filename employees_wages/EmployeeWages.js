207e9a7056cec7212e168fdf123b3f8813138d9b
Manish <patelmanish131149198@gmail.com>
2025-03-04 21:49:55 +0530

    [Manish]Add. calculate wages for a month feature added

--- employees_wages/EmployeeWages.js
+++ employees_wages/EmployeeWages.js
@@ -17,21 +17,21 @@ const PART_TIME_HOURS = 4;
 const FULL_TIME_HOURS = 8;
 const WAGE_PER_HOUR = 20;
 
-let empHrs = 0;
-empCheck = Math.floor(Math.random() * 10) % 3;
+// let empHrs = 0;
+// empCheck = Math.floor(Math.random() * 10) % 3;
 
-switch(empCheck){
-    case IS_PART_TIME: 
-        empHrs = PART_TIME_HOURS;
-        break;
-    case IS_FULL_TIME: 
-        empHrs = FULL_TIME_HOURS;
-        break;
-    default : 
-        empHrs = 0;
-}
-let empWage = empHrs * WAGE_PER_HOUR;
-console.log("Employee Wage: " + empWage);
+// switch(empCheck){
+//     case IS_PART_TIME: 
+//         empHrs = PART_TIME_HOURS;
+//         break;
+//     case IS_FULL_TIME: 
+//         empHrs = FULL_TIME_HOURS;
+//         break;
+//     default : 
+//         empHrs = 0;
+// }
+// let empWage = empHrs * WAGE_PER_HOUR;
+// console.log("Employee Wage: " + empWage);
 
 // UC3 to get working hours
 function getWorkingHours(empCheck) {
@@ -46,3 +46,21 @@ function getWorkingHours(empCheck) {
             empHrs = 0;
     }
 }
+let empHrs = 0;
+empCheck = Math.floor(Math.random() * 10) % 3;
+empHrs = getWorkingHours(empCheck);
+let empWage = empHrs * WAGE_PER_HOUR;
+console.log("Employee Wage: " + empWage);
+
+
+// UC 4 Calculating wages for a month
+const NUM_OF_WORKING_DAYS = 2;
+empHrs = 0;
+
+for (let day = 0; day < NUM_OF_WORKING_DAYS; day++) {
+    let empCheck = Math.floor(Math.random() * 10) % 3;
+    empHrs += getWorkingHours(empCheck);
+}
+
+empWage = empHrs * WAGE_PER_HOUR;
+console.log("Total hours: " + empHrs + " Employee Wage: " + empWage);
